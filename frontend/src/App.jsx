262a663887d50083bca96dd862eacc6b200d1be8
Memet <exortek979@gmail.com>
2025-07-10 03:46:39 +0300

    Add routing and layout to App component
    Integrated React Router with routes for Readable, Writable, and Duplex pages. Added basic layout and styling to the App component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Readable, Writable, Duplex } from '@pages';
+
 function App() {
-  return <></>;
+  return (
+    <div className={'w-full py-10 px-4 min-h-screen bg-gray-900 text-white'}>
+      <div className={'w-full max-w-6xl mx-auto'}>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path={'/readable'}
+              element={<Readable />}
+            />
+            <Route
+              path={'/writable'}
+              element={<Writable />}
+            />
+            <Route
+              path={'/duplex'}
+              element={<Duplex />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
 }
 
 export default App;
