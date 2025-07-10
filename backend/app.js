ef3580bff389ce0ab2f9be31087e7efd2997f57a
Memet <exortek979@gmail.com>
2025-07-10 03:44:58 +0300

    Add custom error handler middleware to app
    Imported and registered the customError middleware as the global error handler in the Fastify app to improve error handling.

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,15 @@ const fastifyCors = require('@fastify/cors');
 
 const routes = require('./routes');
 
+const { customError } = require('./middlewares');
+
 const app = fastify({ logger: true });
 
 app.register(fastifyCors, {
   origin: '*',
 });
 app.register(routes, { prefix: '/api/v1' });
+
+app.setErrorHandler(customError);
 
 module.exports = app;
