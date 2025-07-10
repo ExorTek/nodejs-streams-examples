475191314312ea6e2f7619b12eac2ded2243aa98
Memet <exortek979@gmail.com>
2025-07-10 03:46:44 +0300

    Create index.js

---
+++ frontend/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as StreamCard } from './StreamCard';
+export { default as StreamControls } from './StreamControls';
+export { default as StreamStats } from './StreamStats';
+export { default as StreamOutput } from './StreamOutput';
+export { default as Header } from './Header';
+export { default as InfoPanel } from './InfoPanel';
